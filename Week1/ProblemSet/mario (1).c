5327ff9fb5d2c28f8aac0dc4f38b41a056d8fdf1
ththanh <hoaithanhtrinh2002@gmail.com>
2023-09-09 17:07:25 +0700

    lab week 2

---
+++ Week1/ProblemSet/mario (1).c
@@ -0,0 +1,40 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("N: ");
+    }
+    while (n < 1 || n > 8);
+
+    for (int i = 1; i <= n; i++)
+    {
+        // Print leading spaces
+        for (int j = 1; j <= n - i; j++)
+        {
+            printf(" ");
+        }
+
+        // Print '#' characters
+        for (int j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+
+        for (int k = 0; k < 2; k++)
+        {
+            printf(" ");
+        }
+
+        for (int k = 0; k < i; k++)
+        {
+            printf("#");
+        }
+
+        // Move to the next line
+        printf("\n");
+    }
+}
