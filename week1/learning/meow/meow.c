585193c76191026b9f00da4f223c07f1633d897b
Ngan LE <ngan.ltkn2911@gmail.com>
2023-09-16 23:07:30 +0200

    added meow

--- week1/learning/meow/meow.c
+++ week1/learning/meow/meow.c
@@ -3,5 +3,25 @@
 
 int main(void)
 {
-    printf("meow\n");
+    // Loop 1
+    int i = 3; 
+    while (i > 0)
+    {
+        printf("meow\n");
+        i--;
+    }
+
+    // Loop 2
+    for (int i = 0; i < 3; i++)
+    {
+        printf("meow meow\n");
+    }
+
+    // Loop 3
+    int j = 0;
+    while (j < 3)
+    {
+        printf("meow meow meow\n");
+        j++;
+    }
 }
