b822207c2568c44010613810e9c25b295e5a98dd
Ngan LE <ngan.ltkn2911@gmail.com>
2023-09-17 00:12:09 +0200

    added more operators for calculator.c

--- week1/learning/operators/calculator.c
+++ week1/learning/operators/calculator.c
@@ -3,8 +3,34 @@
 
 int main(void)
 {
+    // operator 1
+    printf("Sum\n");
     int x = get_int("x: ");
     int y = get_int("y: ");
 
-    printf("%i\n", x + y);
+    printf("sum: %i\n", x + y);
+
+    // operator 2 
+    printf("Division\n");
+    long a = get_long("a: ");
+    long b = get_long("b: ");
+
+    printf("division: %li\n", a / b);
+
+    // operator 3
+    printf("Division with float\n");
+    long n = get_long("n: ");
+    long m = get_long("m: ");
+
+    float l = (float) n / (float) m; 
+    printf("result 1: %f\n", l);
+
+    // operator 4 => floating-point imprecision 
+    printf("Division with double\n");
+    long a1 = get_long("a1: ");
+    long b1 = get_long("b1: ");
+
+    // "double" is at least more precise than using "float"
+    double c = (double) a1 / (double) b1; 
+    printf("result 2: %.20f\n", c);
 }
