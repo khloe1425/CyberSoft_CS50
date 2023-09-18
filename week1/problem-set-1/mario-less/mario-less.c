a866c501a2603393dcc6adce2adad1d625bafd21
Ngan LE <ngan.ltkn2911@gmail.com>
2023-09-18 23:53:01 +0200

    built a left-aligned pyramid first

--- week1/problem-set-1/mario-less/mario-less.c
+++ week1/problem-set-1/mario-less/mario-less.c
@@ -3,5 +3,18 @@
 
 int main(void) 
 {
+    int height; 
+    do
+    {
+        height = get_int("Height: ");
+    } while (height < 1 || height > 8);
     
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
