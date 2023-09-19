79e6a3d045ed0a294a440bea124ddb17666428f7
Ngan LE <ngan.ltkn2911@gmail.com>
2023-09-20 00:04:08 +0200

    resolved mario-more

--- week1/problem-set-1/mario-more/mario-more.c
+++ week1/problem-set-1/mario-more/mario-more.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <cs50.h>
 
+void pyramid(int n);
+
 int main(void)
 {
     int height; 
@@ -9,5 +11,40 @@ int main(void)
         height = get_int("Height: ");
     } while (height < 1 || height > 8);
 
-    
+    pyramid(height);
+}
+
+// TODO: Using 3 loops to print # or space with condition
+void pyramid(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n + 1; j++)
+        {
+            if ((n - 1) - j > i)
+            {
+                printf(" ");
+            }
+            else if (j == n)
+            {
+                printf("  ");
+            }
+            else 
+            {
+                printf("#");
+            }
+        }
+        for (int z = 0; z < n; z++)
+        {
+            if (z <= i)
+            {
+                printf("#");
+            }
+            else 
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
 }
