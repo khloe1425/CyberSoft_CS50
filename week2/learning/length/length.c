54959d9a5f00a1dcc766097fe00e2b0ed56252de
Ngan LE <ngan.ltkn2911@gmail.com>
2023-09-30 21:29:17 +0200

    imported string.h library

--- week2/learning/length/length.c
+++ week2/learning/length/length.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main(void)
 {
     string name = get_string("What's your name? ");
 
-    int n = 0;
+    /* int n = 0;
     while (name[n] != '\0')
     {
         n++;
     }
+    printf("%i\n", n); */
+
+    int n = strlen(name);
     printf("%i\n", n);
 }
