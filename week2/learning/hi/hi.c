4c557e145beb6494f572e786bd68b86823a74071
Ngan LE <ngan.ltkn2911@gmail.com>
2023-09-30 21:21:31 +0200

    added hi & no-vowels folders

---
+++ week2/learning/hi/hi.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    char c1 = 'H';
+    char c2 = 'I';
+    char c3 = '!';
+
+    printf("%c%c%c\n", c1, c2, c3);
+    printf("%i %i %i\n", c1, c2, c3);
+
+    string s = "HI!";
+    printf("%s\n", s);
+    printf("%c%c%c\n", s[0], s[1], s[2]);
+    printf("%i %i %i %i\n", s[0], s[1], s[2], s[3]);
+
+    string words[2];
+    words[0] = "HI!";
+    words[1] = "BYE!";
+
+    printf("%c%c%c\n", words[0][0], words[0][1], words[0][2]);
+    printf("%c%c%c%c\n", words[1][0], words[1][1], words[1][2], words[1][3]);
+}
